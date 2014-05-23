38c0e6bb05ecb630545bfdcef519cb52ce76cc13
Hanno <hcombrinck@gmail.com>
2014-05-23 11:11:45 +0200

    Added initial logging support

---
+++ Source/Logging/Log.cpp
@@ -0,0 +1,89 @@
+#include "Log.h"
+#include <time.h>
+
+namespace baselib {
+
+	LOG_LEVEL Logger::m_LogLevel = LOGLEVEL_VERBOSE;
+	bool Logger::m_bAddLogLevel = false;
+	bool Logger::m_bAddTimeStamp = false;
+
+	Logger::Logger()
+	{
+	}
+
+	Logger::~Logger()
+	{
+	   m_OutStream << endl;
+	   cout << m_OutStream.str();
+	}
+
+	string Logger::LevelToString(LOG_LEVEL level)
+	{
+		string logLevelStr = "";
+		switch (level)
+		{
+		case LOGLEVEL_ERROR: logLevelStr = "Error"; break;
+		case LOGLEVEL_WARNING: logLevelStr = "Warning"; break;
+		case LOGLEVEL_INFO: logLevelStr = "Info"; break;
+		case LOGLEVEL_DEBUG_INFO: logLevelStr = "Debug Info"; break;
+		case LOGLEVEL_VERBOSE: logLevelStr = "Verbose"; break;
+		default: assert(false); 
+		}
+		return logLevelStr;
+	}
+
+	// From http://msdn.microsoft.com/en-us/library/a442x3ye.aspx
+	static const string currentDateTime() {
+		struct tm newtime;
+		char am_pm[] = "AM";
+		__time64_t long_time;
+		char timebuf[26];
+		errno_t err;
+
+		// Get time as 64-bit integer.
+		_time64( &long_time ); 
+		// Convert to local time.
+		err = _localtime64_s( &newtime, &long_time ); 
+		if (err)
+		{
+			printf("Invalid argument to _localtime64_s.");
+			exit(1);
+		}
+		if( newtime.tm_hour > 12 )        // Set up extension. 
+				strcpy_s( am_pm, sizeof(am_pm), "PM" );
+		if( newtime.tm_hour > 12 )        // Convert from 24-hour 
+				newtime.tm_hour -= 12;    // to 12-hour clock. 
+		if( newtime.tm_hour == 0 )        // Set hour to 12 if midnight.
+				newtime.tm_hour = 12;
+
+		// Convert to an ASCII representation. 
+		err = asctime_s(timebuf, 26, &newtime);
+		if (err)
+		{
+		   printf("Invalid argument to asctime_s.");
+		   exit(1);
+		}
+		timebuf[19] = '\0';
+		return string(timebuf) + " " + string(am_pm);
+	}
+
+	ostringstream& Logger::GetStream(LOG_LEVEL level)
+	{
+		if (GetAddTimeStamp())
+		{
+			m_OutStream << "[" << currentDateTime();
+
+			if (GetAddLogLevel())
+				m_OutStream << " - " << LevelToString(level) << "]:\t";
+			else
+				m_OutStream << "]:\t";
+		}
+		else if (GetAddLogLevel())
+		{
+			m_OutStream << "[" << LevelToString(level) << "]:\t";
+		}
+		
+		return m_OutStream;
+	}
+
+}
---
+++ Source/Logging/Log.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <sstream>
+#include <string>
+#include <iostream>
+#include <assert.h>
+
+using namespace std;
+
+namespace baselib
+{
+	enum LOG_LEVEL
+	{
+		LOGLEVEL_NONE,
+		LOGLEVEL_ERROR,
+		LOGLEVEL_WARNING,
+		LOGLEVEL_INFO,
+		LOGLEVEL_DEBUG_INFO,
+		LOGLEVEL_VERBOSE
+	};
+
+	class Logger
+	{
+	public:
+		Logger();
+		~Logger();
+		
+		ostringstream& GetStream(LOG_LEVEL level);
+
+		static void SetLogLevel(LOG_LEVEL eLevel) { m_LogLevel = eLevel; }
+		static LOG_LEVEL GetLogLevel() { return m_LogLevel; }
+		
+		static void SetAddTimeStamp(bool b) { m_bAddTimeStamp = b; }
+		static bool GetAddTimeStamp() { return m_bAddTimeStamp; }
+
+		static void SetAddLogLevel(bool b) { m_bAddLogLevel = b; }
+		static bool GetAddLogLevel() { return m_bAddLogLevel; }
+
+	private:
+		string LevelToString(LOG_LEVEL level);
+
+		ostringstream m_OutStream;
+
+		static LOG_LEVEL m_LogLevel;
+		static bool m_bAddTimeStamp;
+		static bool m_bAddLogLevel;
+	};
+
+	#define LOG_ERROR \
+		if (Logger::GetLogLevel() < LOGLEVEL_ERROR) ;\
+		else Logger().GetStream(LOGLEVEL_ERROR) 
+
+	#define LOG_WARNING \
+		if (Logger::GetLogLevel() < LOGLEVEL_WARNING) ;\
+		else Logger().GetStream(LOGLEVEL_WARNING) 
+
+	#define LOG_INFO \
+		if (Logger::GetLogLevel() < LOGLEVEL_INFO) ;\
+		else Logger().GetStream(LOGLEVEL_INFO) 
+
+	#define LOG_DEBUG \
+		if (Logger::GetLogLevel() < LOGLEVEL_DEBUG_INFO) ;\
+		else Logger().GetStream(LOGLEVEL_DEBUG_INFO)
+
+	#define LOG_VERBOSE \
+	if (Logger::GetLogLevel() < LOGLEVEL_VERBOSE) ;\
+		else Logger().GetStream(LOGLEVEL_VERBOSE)
+}
