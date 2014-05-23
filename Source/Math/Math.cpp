f142b628ecd747ea9473508931f6abd68a6337f1
Hanno <hcombrinck@gmail.com>
2014-05-23 18:56:22 +0200

    Added math helpers - includes glm library

---
+++ Source/Math/Math.cpp
@@ -0,0 +1,65 @@
+#include "Math.h"
+
+namespace baselib {
+
+	void calcQuatWComponent(glm::quat &q) 
+	{
+		float t = 1.0f - q.x*q.x - q.y*q.y - q.z*q.z;
+		q.w = (t < 0.0f) ? 0.0f : -sqrt(t);
+	}
+
+	glm::quat multiplyQuat(glm::quat &q1, glm::quat &q2) 
+	{
+		Vec3 v1(q1.x, q1.y, q1.z);
+		Vec3 v2(q2.x, q2.y, q2.z);
+
+		float s = q1.w*q2.w - glm::dot(v1,v2);
+		Vec3 v = (glm::cross(v1,v2) + (v2*q1.w) ) + (v1*q2.w);
+
+		return glm::quat(s, v.x, v.y, v.z);
+	}
+
+	glm::quat slerpQuat(glm::quat &q1, glm::quat &q2, float u) 
+	{
+		float cosOmega = glm::dot(q1, q2);
+		if (cosOmega < 0.0f) 
+		{
+			q2.w = q2.w*-1;
+			q2.x = q2.x*-1;
+			q2.y = q2.y*-1;
+			q2.z = q2.z*-1;
+			cosOmega = -cosOmega;
+		}
+
+		float k0, k1;
+		if (cosOmega > 0.99999f) 
+		{ //if small angle then do norml linear interp. instead of spherical
+			k0 = 1.0f - u;
+			k1 = u;
+		}
+		else 
+		{
+			float sinOmega = sqrt(1.0f - cosOmega*cosOmega);
+			float omega = atan2(sinOmega, cosOmega);
+
+			float sinOmegaInv = 1.0f/sinOmega;
+
+			k0 = sin((1.0f - u)*omega)*sinOmegaInv;
+			k1 = sin(u*omega)*sinOmegaInv;
+		}
+
+		glm::quat result(q1.w*k0 + q2.w*k1, q1.x*k0 + q2.x*k1, q1.y*k0 + q2.y*k1, q1.z*k0 + q2.z*k1);
+		result = glm::normalize(result);
+		return result;
+	}
+
+	float toRad(float deg)
+	{
+		return deg*PI/180.0f;
+	}
+
+	float toDeg(float rad)
+	{
+		return rad*180.0f/PI;
+	}
+}
---
+++ Source/Math/Math.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/quaternion.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+namespace baselib
+{
+	typedef glm::vec2 Vec2;
+	typedef glm::vec3 Vec3;
+	typedef glm::vec4 Vec4;
+	typedef glm::mat4 Mat4;
+
+	const float PI = 3.14159265f;
+
+	//! Assumes unit quaternion and calculate w component.
+	void calcQuatWComponent(glm::quat &q);
+	//! Multiply q1 with q2.
+	glm::quat multiplyQuat(glm::quat &q1, glm::quat &q2);
+	//! Calculates spherical linear interpolation between quaternions.
+	glm::quat slerpQuat(glm::quat &q1, glm::quat &q2, float u);
+	//! Converts degrees to radians.
+	float toRad(float deg);
+	//! Converts radians to degrees
+	float toDeg(float rad);
+
+	//! Clamps val between minVal and maxVal.
+	template <class T>
+	static void clampVal(T& val, T minVal, T maxVal)
+	{
+		if (val >= maxVal)
+			val = maxVal;
+		else if (val <= minVal)
+			val = minVal;
+	}
+
+	//! Smooths current value toward target value.
+	template <class T>
+	static void smoothTransition(const T& target, T &current, float speed)
+	{
+		if (glm::length(target - current) > 0.01f)
+			current += (target - current)*speed;
+		else 
+			current = target;
+	}
+}
