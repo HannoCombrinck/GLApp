951ea30021bcba42ec434c96536289dd41e19bb7
Hanno <hcombrinck@gmail.com>
2014-05-25 18:15:15 +0200

    Added Material getters and setters.

--- Source/Graphics/Material.h
+++ Source/Graphics/Material.h
@@ -23,6 +23,21 @@ namespace baselib
 			Material();
 			//! Destructor
 			~Material();
+
+			//! Set the shader instance.
+			void setShader(const boost::shared_ptr<Shader>& spShader) { m_spShader = spShader; }
+			//! Getter for setShader().
+			boost::shared_ptr<Shader> getShader() const { return m_spShader; }
+
+			//! Set the texture.
+			void setTexture(const boost::shared_ptr<Texture>& spTexture) { m_spTexture = spTexture; }
+			//! Getter for setTexture().
+			boost::shared_ptr<Texture> getTexture() const { return m_spTexture; }
+
+			//! Set the render state.
+			void setRenderState(const boost::shared_ptr<RenderState>& spRenderState) { m_spRenderState = spRenderState; }
+			//! Getter for setRenderState().
+			boost::shared_ptr<RenderState> getRenderState() const { return m_spRenderState; }
 		
 		private:
 			boost::shared_ptr<Shader> m_spShader;			//!< Shader used by this material.
