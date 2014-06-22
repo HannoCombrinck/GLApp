135a50f0b179dcacddff728e0849e9a26406e460
Hanno <hcombrinck@gmail.com>
2014-06-22 22:55:54 +0200

    Implemented bare bones default VisualCollector.

--- Source/Graphics/VisualCollector.cpp
+++ Source/Graphics/VisualCollector.cpp
@@ -1,6 +1,9 @@
 #include "VisualCollector.h"
 
 #include <Logging/Log.h>
+#include <Graphics/Node.h>
+#include <Graphics/Visual.h>
+#include <boost/range/algorithm/sort.hpp>
 
 namespace baselib { namespace graphics {
 
@@ -12,6 +15,21 @@ namespace baselib { namespace graphics {
 	VisualCollector::~VisualCollector()
 	{
 		LOG_VERBOSE << "VisualCollector destructor";
+	}
+
+	void VisualCollector::collect(const boost::shared_ptr<Node>& spNode)
+	{
+		m_apVisuals.clear();
+
+		// Traverse Node hierarchy and selectively add visuals
+		std::vector<Visual*>& apVisuals = m_apVisuals;
+		spNode->forEach([&apVisuals](const boost::shared_ptr<Node>& spNode) {
+			if (auto spVisual = spNode->getVisual()) 
+				apVisuals.push_back(spVisual.get()); // TODO: Only add visuals if they aren't culled/filtered
+		});
+
+		// Sort m_apVisuals
+		boost::sort(m_apVisuals, [](const Visual* pLHS, const Visual* pRHS) { return true; /*TODO: Write visual sorting predicate*/ });
 	}
 
 } }
--- Source/Graphics/VisualCollector.h
+++ Source/Graphics/VisualCollector.h
@@ -1,5 +1,17 @@
 #pragma once
 
+#include <boost/shared_ptr.hpp>
+#include <vector>
+
+namespace baselib 
+{
+	namespace graphics
+	{
+		class Node;
+		class Visual;
+	}
+}
+
 namespace baselib 
 {
 	namespace graphics
@@ -15,7 +27,15 @@ namespace baselib
 			//! Destructor.
 			virtual ~VisualCollector();
 
+			//! Collect visuals from Node hierarchy 
+			void collect(const boost::shared_ptr<Node>& spNode);
+
+			//! Get the sorted list of visuals.
+			const std::vector<Visual*>& getVisuals() const { return m_apVisuals; }
+
 		private:
+			//! List of Visuals sorted according to rendering order
+			std::vector<Visual*> m_apVisuals; // Using a normal pointer to avoid overhead of locking a weak_ptr for every draw call - look at boost::intrusive as alternative
 
 		};
 	}
