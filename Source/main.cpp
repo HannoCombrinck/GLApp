6328298c50446d63cd68ca30cd718f9c84300e69
Hanno Combrinck <hcombrinck@gmail.com>
2019-10-15 10:22:52 +0200

    Removed temporary code and let main launch deferred rendering test app.

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,15 +1,16 @@
-#include <ModelViewer/ModelViewer.h>
+#include <TestApps/DeferredRenderingApp.h>
 
 using namespace std;
+using namespace baselib;
 
 int main(int arc, char** argv)
 {
-	ModelViewer t;
-	t.setPosition(100, 100);
-	t.setSize(512, 512);
-	t.setFullScreen(false);
-	t.setWindowTitle("ModelViewer");
-	t.start();
+	DeferredRenderingApp app;
+	app.setPosition(100, 100);
+	app.setSize(512, 512);
+	app.setFullScreen(false);
+	app.setWindowTitle("Deferred rendering test");
+	app.start();
 
 	return 0;
 }
